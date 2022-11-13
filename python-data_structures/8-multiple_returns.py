fd1515e8c6e57e5a65d8f7bf116e17aa3013baca
fesembile <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@github.com>
2022-11-13 01:17:06 -0800

    .

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+def multiple_returns(sentence):
+
+        if sentence:
+
+                    sentence_length = len(sentence)
+
+                        else:
+
+                                    sentence_length = 0
+
+                                        return tuple((sentence_length, None if not sentence else sentence[:1]))
