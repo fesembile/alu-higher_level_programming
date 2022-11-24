37bdf8a17585d855770ca19d853c40dfbd46b819
fesembile <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@github.com>
2022-11-24 08:16:21 -0800

    .

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-"""
-Module 0-hbtn_status.py
-"""
+"""Defines an object attribute lookup function."""
 
-import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:         html = response.read()
-         print('Body response:')
-         print("\t- type: {}".format(type(html)))
-         print("\t- content: {}".format(html))
-         print("\t- utf8 content: {}".format(html.decode('utf-8')))
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
