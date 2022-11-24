38f75a1112639158de4f8beee23234bba64fb8dc
fesembile <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@github.com>
2022-11-24 08:45:38 -0800

    .

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Defines a class-checking function."""
+"""Defines an inherited class-checking function."""
 
 
-def is_same_class(obj, a_class):
-    """Check if an object is exactly an instance of a given class.
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
     Args:
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is exactly an instance of a_class - True.
-        Otherwise - False.
-    """
-    if type(obj) == a_class:
-        return True
-    return False
+        Returns:
+            If obj is an inherited instance of a_class - True.
+            Otherwise - False.
+        """
+        if issubclass(type(obj), a_class) and type(obj) != a_class:
+            return True
+        return False
