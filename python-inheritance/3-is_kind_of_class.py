33e86c8e140e171b6e7cf0da68f302642a67e1d0
fesembile <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@github.com>
2022-11-24 08:05:59 -0800

    .

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,7 +6,7 @@ Module 0-hbtn_status.py
 import urllib.request
 
 if __name__ == "__main__":
-     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:         html = response.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:         html = response.read()
          print('Body response:')
          print("\t- type: {}".format(type(html)))
          print("\t- content: {}".format(html))
