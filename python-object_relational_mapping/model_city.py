05e933b6549930086421a50d73941dbccbe01c75
Fese Mbile <109585199+fesembile@users.noreply.github.com>
2023-01-24 10:17:18 +0000

    .

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""Definition of the City class"""
+'''Using the SQL Alchemy in defining a database class'''
 
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-
 Base = declarative_base()
 
 
 class City(Base):
-    """Class City"""
-
+    '''A class defination of a table in the sql'''
     __tablename__ = 'cities'
-    id = Column(Integer, autoincrement=True,
-                primary_key=True, nullable=False, unique=True)
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
