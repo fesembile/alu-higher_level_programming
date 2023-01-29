d60e94217418e4579e2f9b80f82f5437797dd5dc
Fese Mbile <109585199+fesembile@users.noreply.github.com>
2023-01-29 17:09:37 +0000

    .

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if (len(sys.argv) > 1):
         for i in range(1, len(sys.argv)):
             result += (int(sys.argv[i]))
-   print("{:d}".format(result))print("{:d}
-   
+    print("{:d}".format(result))
+
 # import sys
 # print("{:d}".format(sum(int(n) for n in sys.argv[1:])))
